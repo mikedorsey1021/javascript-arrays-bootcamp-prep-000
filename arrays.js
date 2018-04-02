0d44502c501bd78814087eeae0abd8d782271368
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-02 23:40:06 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray (array, element){
+var newArray = new Array();
+  return newArray.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray (array, element){
+  var newArray = new Array();
+  return newArray.push(element)
+}
+
+function destructivelyAddElementToEndOfArray (){
+  return array.push(element)
+}
+
+function accessElementInArray (array, index){
+  var array = array;
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray (array){
+  var newArray = array
+  newArray.slice(1)
+}
+
