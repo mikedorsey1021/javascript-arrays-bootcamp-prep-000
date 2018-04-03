27778bdab94841c9870a3040d3e1f57e693f51e9
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-03 02:18:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,22 +2,21 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array, element){
 var newArray = []
-var newArray2 = [element, ...newArray]
-return newArray2
+return newArray.unshift()
 
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  return array.unshift(element)
+  return array.unshift()
 }
 
 function addElementToEndOfArray (array, element){
   var newArray = []
-  return newArray.push(element)
+  return newArray.push()
 }
 
 function destructivelyAddElementToEndOfArray (array, element){
-  return array.push(element)
+  return array.push()
 }
 
 function accessElementInArray (array, index){
